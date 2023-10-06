7b4d8d6ccc47ddaff77cf9d73f6036c64e3ed12c
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-10-06 11:08:58 +0200

    feat: Support PodDisruptionBudgets (#27)
    * feat: Support PodDisruptionBudgets
    
    * changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,9 +8,9 @@ All notable changes to this project will be documented in this file.
 
 - Set explicit resources on all containers ([#14]).
 - Support `podOverrides` ([#15]).
-- Operator-rs: `0.44.0` ([#18]).
 - Increase the size limit of the log volumes ([#18])
 - Configuration overrides for the JVM security properties, such as DNS caching ([#23]).
+- Support PodDisruptionBudgets ([#27]).
 
 ### Changed
 
@@ -21,3 +21,4 @@ All notable changes to this project will be documented in this file.
 [#18]: https://github.com/stackabletech/hello-world-operator/pull/18
 [#21]: https://github.com/stackabletech/hello-world-operator/pull/21
 [#23]: https://github.com/stackabletech/hello-world-operator/pull/23
+[#27]: https://github.com/stackabletech/hello-world-operator/pull/27
---
+++ rust/operator-binary/src/operations/pdb.rs
@@ -0,0 +1,63 @@
+use snafu::{ResultExt, Snafu};
+use stackable_operator::{
+    builder::pdb::PodDisruptionBudgetBuilder, client::Client, cluster_resources::ClusterResources,
+    commons::pdb::PdbConfig, kube::ResourceExt,
+};
+
+use crate::{
+    controller::HELLO_CONTROLLER_NAME,
+    crd::{HelloCluster, HelloRole, APP_NAME},
+    OPERATOR_NAME,
+};
+
+#[derive(Snafu, Debug)]
+pub enum Error {
+    #[snafu(display("Cannot create PodDisruptionBudget for role [{role}]"))]
+    CreatePdb {
+        source: stackable_operator::error::Error,
+        role: String,
+    },
+    #[snafu(display("Cannot apply PodDisruptionBudget [{name}]"))]
+    ApplyPdb {
+        source: stackable_operator::error::Error,
+        name: String,
+    },
+}
+
+pub async fn add_pdbs(
+    pdb: &PdbConfig,
+    hello: &HelloCluster,
+    role: &HelloRole,
+    client: &Client,
+    cluster_resources: &mut ClusterResources,
+) -> Result<(), Error> {
+    if !pdb.enabled {
+        return Ok(());
+    }
+    let max_unavailable = pdb.max_unavailable.unwrap_or(match role {
+        HelloRole::Server => max_unavailable_servers(),
+    });
+    let pdb = PodDisruptionBudgetBuilder::new_with_role(
+        hello,
+        APP_NAME,
+        &role.to_string(),
+        OPERATOR_NAME,
+        HELLO_CONTROLLER_NAME,
+    )
+    .with_context(|_| CreatePdbSnafu {
+        role: role.to_string(),
+    })?
+    .with_max_unavailable(max_unavailable)
+    .build();
+    let pdb_name = pdb.name_any();
+    cluster_resources
+        .add(client, pdb)
+        .await
+        .with_context(|_| ApplyPdbSnafu { name: pdb_name })?;
+
+    Ok(())
+}
+
+fn max_unavailable_servers() -> u16 {
+    1
+}
